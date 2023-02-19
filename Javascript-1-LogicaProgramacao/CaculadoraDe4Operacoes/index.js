91bd57ad286244355d1940a2731d0f709202df34
Lucas Pina <ti.lucaspina@gmail.com>
2023-02-19 14:32:14 -0300

    exercicios

--- Javascript-1-LogicaProgramacao/CaculadoraDe4Operacoes/index.js
+++ Javascript-1-LogicaProgramacao/CaculadoraDe4Operacoes/index.js
@@ -4,3 +4,19 @@ Escreva um programa em js que permita inserir dois valores numéricos e então c
 
 Após calcular os resultados o programa deve exíbi-los na tela 
 */
+
+const primeiroNumero = parseFloat(prompt("Digite primeiro numero:"));
+const segundoNumero = parseFloat(prompt("Digite segundo numero:"));
+
+const soma = primeiroNumero + segundoNumero;
+const subtracao = primeiroNumero - segundoNumero;
+const multiplicacao = primeiroNumero * segundoNumero;
+const divisao = primeiroNumero / segundoNumero;
+
+alert(`
+    o Resultados das operações sao:
+    soma: ${soma}
+    subtração: ${subtracao}
+    multiplicação: ${multiplicacao}
+    divião: ${divisao}
+`);
