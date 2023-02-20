db3fc15cbc564d7eb91d2b9b699e33f73a11dd7b
Lucas Pina <ti.lucaspina@gmail.com>
2023-02-20 18:10:57 -0300

    exercicios

--- Javascript-1-LogicaProgramacao/CalculoDeDano/index.js
+++ Javascript-1-LogicaProgramacao/CalculoDeDano/index.js
@@ -11,3 +11,72 @@ Escreva um programa em js que permita inserir o nome e o poder de uma personagem
 Por fim, o programa deve subtrair a quantidade de dano da quantidade de pontos de vida do personagem defensor e exibir na tela a quantidade de dano e as informaç~es atualizadas de ambos os personagen.
 
 */
+
+const personagemAtacante = prompt("Digite o nome do personagem atacante:");
+const poderAtaquePersonagemAtacante = parseFloat(
+  prompt("Digite o poder de ataque do personagem atacante:")
+);
+
+const personagemDefensor = prompt("Digite o nome do personagem defensor:");
+let pontosVidaPersonagemDefensor = parseFloat(
+  prompt("Digite os pontos de vida do personagem defensor:")
+);
+
+const poderDefesaDefensor = parseFloat(
+  prompt("Digite o poder de defesa do personagem defensor:")
+);
+
+const escudoPersonagem = prompt("Defensor possui escudo?:");
+
+
+const atacanteGanhaDefensorSemEscudo =
+personagemAtacante > poderDefesaDefensor && escudoPersonagem === "Não";
+const atacanteGanhaDefensorTemEscudo =
+personagemAtacante > poderDefesaDefensor && escudoPersonagem === "Sim";
+const naoCausaDano = personagemAtacante === poderDefesaDefensor;
+
+const dano = 0;
+
+if (atacanteGanhaDefensorSemEscudo) {
+  dano = personagemAtacante - poderDefesaDefensor;
+  alert(`
+    informações personagens :
+
+    personagem atacante(${personagemAtacante}):
+    ataque: ${poderAtaquePersonagemAtacante}
+
+    -------------------------------------------
+    personagem defensor(${personagemDefensor}):
+    vida: ${pontosVidaPersonagemDefensor}
+    defesa: ${poderDefesaDefensor}
+    dano sofrido: ${dano}
+  `);
+} else if (atacanteGanhaDefensorTemEscudo) {
+  dano = personagemAtacante - poderDefesaDefensor / 2;
+  alert(`
+  informações personagens :
+
+  personagem atacante(${personagemAtacante}):
+  ataque: ${poderAtaquePersonagemAtacante}
+
+  -------------------------------------------
+  personagem defensor(${personagemDefensor}):
+  vida: ${pontosVidaPersonagemDefensor - dano}
+  defesa: ${poderDefesaDefensor}
+  dano sofrido: ${dano}
+`);
+} else if (naoCausaDano) {
+  dano = 0;
+  alert(`
+  informações personagens :
+
+  personagem atacante(${personagemAtacante}):
+  ataque: ${poderAtaquePersonagemAtacante}
+
+  -------------------------------------------
+  personagem defensor(${personagemDefensor}):
+  vida: ${pontosVidaPersonagemDefensor - dano}
+  defesa: ${poderDefesaDefensor}
+  dano sofrido: ${dano}
+`);
+}
